279fa67197c27fd67b7da993ba9f393e52c82670
Meet Joshi <meetjoshi1112002@gmail.com>
2024-02-12 00:47:03 +0530

    Routes created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom"
+import Home from "./pages/home";
+import About from "./pages/about";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
+import Profile from "./pages/profile";
+
 function App() {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/sign-in" element={<Signin />}/>
+          <Route path="/sign-up" element={<Signup /> }/>
+          <Route path="/profile" element={<Profile />}/>
+        </Routes>
+      </Router>
     </>
   );
 }
